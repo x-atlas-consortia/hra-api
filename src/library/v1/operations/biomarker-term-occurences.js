9d9b344c2c2ca442b56bebf1d056d1376b4414d0
Bruce W. Herr II <bherr@indiana.edu>
2024-01-24 11:09:14 -0500

    Update term occurences and fix biomarker tree model to be backwards compatible

--- src/library/v1/operations/biomarker-term-occurences.js
+++ src/library/v1/operations/biomarker-term-occurences.js
@@ -3,6 +3,13 @@ import query from '../queries/biomarker-term-occurences.rq';
 import { executeFilteredConstructQuery } from '../utils/execute-sparql.js';
 import { formatTermOccurences } from '../utils/format-term-occurences.js';
 
+function reformatResponse(jsonld) {
+  const results = formatTermOccurences(jsonld);
+  results['biomarkers'] = results['http://purl.org/ccf/Biomarker'];
+  delete results['http://purl.org/ccf/Biomarker'];
+  return results;
+}
+
 /**
  * Retrieves biomarker term occurrences
  * @param {Object} filter - An object containing query filters
@@ -10,5 +17,5 @@ import { formatTermOccurences } from '../utils/format-term-occurences.js';
  * @returns {Promise<Object>} - A promise that resolves to biomarker term occurrences
  */
 export async function getBiomarkerTermOccurences(filter, endpoint = 'https://lod.humanatlas.io/sparql') {
-  return formatTermOccurences(await executeFilteredConstructQuery(query, filter, frame, endpoint));
+  return reformatResponse(await executeFilteredConstructQuery(query, filter, frame, endpoint));
 }
--- src/library/v1/operations/biomarker-tree-model.js
+++ src/library/v1/operations/biomarker-tree-model.js
@@ -3,6 +3,31 @@ import query from '../queries/biomarker-tree-model.rq';
 import { executeFilteredConstructQuery } from '../utils/execute-sparql.js';
 import { formatTreeModel } from '../utils/format-tree-model.js';
 
+function reformatResponse(jsonld) {
+  const tree = formatTreeModel(jsonld);
+  const bmTypes = ['gene', 'protein', 'metabolites', 'proteoforms', 'lipids'];
+  
+  // Reformat the root node
+  const rootNode = tree.nodes[tree.root]
+  rootNode.children = bmTypes;
+  rootNode.id = 'biomarkers';
+  rootNode.parent = '';
+  tree.nodes['biomarkers'] = rootNode;
+  delete tree.nodes[tree.root];
+  tree.root = 'biomarkers';
+  
+  // Relink using the labels instead of iris for backwards compatibility
+  for (const type of bmTypes) {
+    const node = tree.nodes[type] = tree.nodes[`http://purl.org/ccf/${type}`];
+    delete tree.nodes[`http://purl.org/ccf/${type}`];
+    node.children.forEach((iri) => {
+      tree.nodes[iri].parent = type;
+    });
+    node.id = type;
+  }
+  return tree;
+}
+
 /**
  * Retrieves the biomarker tree model
  * @param {Object} filter - An object containing query filters
@@ -10,5 +35,5 @@ import { formatTreeModel } from '../utils/format-tree-model.js';
  * @returns {Promise<Object>} - A promise that resolves to reference organ data
  */
 export async function getBiomarkerTreeModel(filter, endpoint = 'https://lod.humanatlas.io/sparql') {
-  return formatTreeModel(await executeFilteredConstructQuery(query, filter, frame, endpoint), 'biomarkers');
+  return reformatResponse(await executeFilteredConstructQuery(query, filter, frame, endpoint));
 }
