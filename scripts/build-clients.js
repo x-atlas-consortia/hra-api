6792121c31256a92bb331fcb731cca3f9aff357e
Bruce W. Herr II <bherr@indiana.edu>
2024-05-02 08:56:11 -0400

    Build clients and print publishing commands

--- scripts/build-clients.js
+++ scripts/build-clients.js
@@ -16,7 +16,19 @@ for (const clientConfig of clients) {
   const name = basename(clientConfig, '.yaml');
   sh.exec(`npx openapi-generator-cli generate -c ${clientConfig} -i ${BUILD_DIR}/spec.yaml -o ${BUILD_DIR}/${name}`);
 
-  if (name !== 'python-client') {
+  if (name === 'python-client') {
+    sh.exec(`cd ${BUILD_DIR}/${name} && /usr/bin/env python setup.py bdist_wheel`);
+  } else {
     sh.exec(`cd ${BUILD_DIR}/${name} && npm install && npm run build`);
+  }
+}
+
+console.log('=== Publishing Commands ===\n');
+for (const clientConfig of clients) {
+  const name = basename(clientConfig, '.yaml');
+  if (name === 'python-client') {
+    console.log(`pushd ${BUILD_DIR}/${name}; twine upload dist/*; popd`); // publishing command
+  } else {
+    console.log(`pushd ${BUILD_DIR}/${name}; npm publish; popd`);
   }
 }
