ba62b106d0bf7169a371273e831f2b12beead2dd
Bruce W. Herr II <bherr@indiana.edu>
2024-06-21 16:00:10 -0400

    Use a job queue when starting ds graph work

--- src/library/v1/utils/dataset-graph/start-dataset-graph-work.js
+++ src/library/v1/utils/dataset-graph/start-dataset-graph-work.js
@@ -1,9 +1,17 @@
+import Queue from 'mini-queue';
 import { Worker } from 'worker_threads';
+
+const QUEUE = new Queue({ activeLimit: 4 });
+
+QUEUE.on('process', (job, jobDone) => {
+  const worker = new Worker('./dist/create-dataset-graph.worker.js', {
+    workerData: job.data,
+  });
+  worker.on('exit', (_exitCode) => jobDone());
+});
 
 export async function startDatasetWork(token, request, endpoint) {
   // TODO: write config / db loading status to blazegraph
 
-  const worker = new Worker('./dist/create-dataset-graph.worker.js', {
-    workerData: { token, request, endpoint },
-  });
+  QUEUE.createJob({ token, request, endpoint });
 }
