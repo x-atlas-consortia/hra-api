f6fa2dd51ba4d4664e485185e1d4621cf294ed75
Andreas Bueckle <abueckle@iu.edu>
2024-07-09 12:38:14 -0400

    Build new version of API and Python client locally, then re-ran

--- scripts/build-clients.js
+++ scripts/build-clients.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+
 import { globSync } from 'glob';
 import { basename } from 'path';
 import sh from 'shelljs';
 
-const BUILD_DIR='./dist/clients';
+const BUILD_DIR = './dist/clients';
 const clients = globSync('./openapi-clients/configs/*.yaml');
 
 sh.mkdir('-p', BUILD_DIR);
@@ -13,24 +14,24 @@ sh.exec(`npm run build:openapi-spec`);
 sh.exec(`node ./scripts/preprocess-openapi.js -i hra-api-spec.yaml -o ${BUILD_DIR}/spec.yaml`);
 
 for (const clientConfig of clients) {
-  const name = basename(clientConfig, '.yaml');
-  sh.exec(`npx openapi-generator-cli generate -c ${clientConfig} -i ${BUILD_DIR}/spec.yaml -o ${BUILD_DIR}/${name}`);
+    const name = basename(clientConfig, '.yaml');
+    sh.exec(`npx openapi-generator-cli generate -c ${clientConfig} -i ${BUILD_DIR}/spec.yaml -o ${BUILD_DIR}/${name}`);
 
-  if (name === 'python-client') {
-    sh.exec(`cd ${BUILD_DIR}/${name} && /usr/bin/env python setup.py bdist_wheel`);
-  } else {
-    sh.exec(`cd ${BUILD_DIR}/${name} && npm install && npm run build`);
-  }
+    if (name === 'python-client') {
+        sh.exec(`cd ${BUILD_DIR}/${name} && /usr/bin/env python setup.py bdist_wheel`);
+    } else {
+        sh.exec(`cd ${BUILD_DIR}/${name} && npm install && npm run build`);
+    }
 }
 
 console.log('=== Publishing Commands ===\n');
 for (const clientConfig of clients) {
-  const name = basename(clientConfig, '.yaml');
-  if (name === 'python-client') {
-    console.log(`pushd ${BUILD_DIR}/${name}; twine upload dist/*; popd # U: __token__`); // publishing command
-  } else if (name === 'angular-client') {
-    console.log(`pushd ${BUILD_DIR}/${name}/dist; npm publish --access public; popd`);
-  } else {
-    console.log(`pushd ${BUILD_DIR}/${name}; npm publish --access public; popd`);
-  }
+    const name = basename(clientConfig, '.yaml');
+    if (name === 'python-client') {
+        console.log(`pushd ${BUILD_DIR}/${name}; twine upload dist/*; popd # U: __token__`); // publishing command
+    } else if (name === 'angular-client') {
+        console.log(`pushd ${BUILD_DIR}/${name}/dist; npm publish --access public; popd`);
+    } else {
+        console.log(`pushd ${BUILD_DIR}/${name}; npm publish --access public; popd`);
+    }
 }
