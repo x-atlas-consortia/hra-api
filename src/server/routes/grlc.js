c6dc1710e1af542a5ea4d822d3bad0bf850dd445
Bruce W. Herr II <bherr@indiana.edu>
2024-08-29 12:30:34 -0400

    Update grlc spec processing

--- src/server/routes/grlc.js
+++ src/server/routes/grlc.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { getGrlcSpec } from '../../library/grlc/grlc-spec.js';
 import { longCache } from '../cache-middleware.js';
 
-const grlcRoute = (req, res, _next) => {
-  res.send(`<!doctype html>
+function grlcHtml(name) {
+  return `<!doctype html>
   <html>
     <head>
       <meta charset="utf-8">
@@ -11,7 +11,7 @@ const grlcRoute = (req, res, _next) => {
     </head>
     <body>
       <rapi-doc
-        spec-url="${req.params.name}-spec.json"
+        spec-url="${name}-spec.json"
         fill-request-fields-with-example="false"
         show-header="false"
         render-style="view"
@@ -24,14 +24,28 @@ const grlcRoute = (req, res, _next) => {
         primary-color="#FF0043"
       ></rapi-doc>
     </body>
-  </html>`);
+  </html>`;
+}
+
+const grlcRoute = (req, res, _next) => {
+  res.send(grlcHtml(req.params.name));
 };
+
+function staticGrlcRoute(name) {
+  return (_req, res, _next) => {
+    res.send(grlcHtml(name));
+  };
+}
 
 const grlcSpecRoute = async (req, res, _next) => {
   const spec = await getGrlcSpec(req.params.name);
   res.json(spec);
 };
 
-const routes = Router().get('/grlc/:name.html', grlcRoute).get('/grlc/:name-spec.json', longCache, grlcSpecRoute);
+const routes = Router()
+  .get('/grlc/', staticGrlcRoute('index'))
+  .get('/grlc', staticGrlcRoute('grlc/index'))
+  .get('/grlc/:name.html', grlcRoute)
+  .get('/grlc/:name-spec.json', longCache, grlcSpecRoute);
 
 export default routes;
