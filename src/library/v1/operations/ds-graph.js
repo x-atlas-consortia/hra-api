542a6ef010bb734fd20220ddbf1cf903761970eb
Bruce W. Herr II <bherr@iu.edu>
2026-03-13 15:30:04 -0400

    Make sure no array values in ds-graph for x/y/z data.

--- src/library/v1/operations/ds-graph.js
+++ src/library/v1/operations/ds-graph.js
@@ -1,13 +1,31 @@
-import query from '../queries/rui-locations.rq';
 import frame from '../frames/rui-locations.jsonld';
+import query from '../queries/rui-locations.rq';
 import { executeFilteredConstructQuery } from '../utils/execute-sparql.js';
 import { ensureGraphArray, normalizeJsonLd } from '../utils/jsonld-compat.js';
 
 function reformatResponse(jsonld) {
-  const results = normalizeJsonLd(ensureGraphArray(jsonld), new Set(['datasets', 'samples', 'sections', 'ccf_annotations']));
+  const results = normalizeJsonLd(
+    ensureGraphArray(jsonld),
+    new Set(['datasets', 'samples', 'sections', 'ccf_annotations']),
+    undefined,
+    new Set([
+      'x_dimension',
+      'y_dimension',
+      'z_dimension',
+      'x_translation',
+      'y_translation',
+      'z_translation',
+      'x_rotation',
+      'y_rotation',
+      'z_rotation',
+      'x_scaling',
+      'y_scaling',
+      'z_scaling',
+    ]),
+  );
   return {
     '@context': 'https://hubmapconsortium.github.io/ccf-ontology/ccf-context.jsonld',
-    '@graph': results
+    '@graph': results,
   };
 }
 
